a3ac6bed0c9fc3556efe0f203d8911d66dbe7a81
ld055651 <ld055651@yimidida.com>
2019-03-28 15:36:25 +0800

    完成页面路由动态加载的基础功能

--- src/container/base/meauManage.vue
+++ src/container/base/meauManage.vue
@@ -4,10 +4,11 @@
       :data="meauList"
       height="100%"
       border
+      @row-dblclick="rowDblclick"
       style="width: 100%">
       <el-table-column
         type="index"
-        width="50">
+        width="40">
       </el-table-column>
       <el-table-column
         prop="name"
@@ -15,59 +16,58 @@
         width="120">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="code"
         label="编码"
-        width="120">
+        width="200">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="level"
         label="级别"
-        width="120">
+        width="60">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="url"
         label="URL地址"
-        width="120">
+        width="250">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="componentUrl"
         label="组件地址"
         width="120">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="iconName"
         label="图标样式"
         width="120">
+        <template slot-scope="scope">
+          <li :class="scope.row.iconName"/>
+        </template>
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="type"
         label="类别"
         width="120">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="parentCode"
         label="父级编码"
         width="120">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="isAuth"
         label="是否权限"
         width="120">
       </el-table-column>
       <el-table-column
-        prop="name"
-        label="菜单排序"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        prop="name"
+        prop="createDate"
+        :formatter="dateFormat"
         label="创建日期"
-        width="120">
+        width="160">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="modifyDate"
         label="修改日期"
-        width="120">
+        :formatter="dateFormat">
       </el-table-column>
       <el-table-column
         fixed="right"
@@ -79,16 +79,38 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog
+      title="菜单信息编辑"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <div>
+        <el-row>
+          <el-col :span="4">姓名:</el-col>
+          <el-col :span="7"><el-input /></el-col>
+          <el-col :span="1">&nbsp;</el-col>
+          <el-col :span="4">编码:</el-col>
+          <el-col :span="7"><el-input /></el-col>
+        </el-row>
+
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 
   import {queryMeauByCondition} from '@/api/modules/base'
+  import Moment from 'moment'
 
   export default {
     data(){
       return {
+        dialogVisible:true,
         meauList:[]
       }
     },
@@ -99,20 +121,32 @@
       handleClick(row){
 
       },
+      handleClose(){
+
+      },
+      // 表格内容双击事件
+      rowDblclick(row, column, event){
+        this.dialogVisible = true;
+      },
       queryMeauByCondition(){
         queryMeauByCondition("{}").then(res=>{
           this.meauList = res.data;
         })
+      },
+      // 时间日期格式转换
+      dateFormat:function(row, column, cellValue, index){
+        // ("YYYY-MM-DD HH:mm:ss");
+        if(cellValue){
+          return Moment(cellValue).format("YYYY-MM-DD HH:mm:ss");
+        }else{
+          return "";
+        }
       }
     }
   }
 </script>
-<style lang="stylus" >
+<style lang="stylus" scoped>
   .div_meau_manage{
     height 100%;
-    .el-table thead{
-      color #444444;
-      font-weight bold
-    }
   }
 </style>
